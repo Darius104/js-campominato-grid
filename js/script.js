87cd7f4ed6dad70607748aff1a72a8cf379040ee
Darius104 <comandarius@gmail.com>
2021-11-23 19:08:05 +0100

    Done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-// Creare in JavaScript una griglia 8x8. Ogni cella della 
-// griglia contiene un numero casuale da 1 a 64. I numeri presenti 
-// nelle celle non devono essere ripetuti (ovvero la griglia contiene **tutti** i numeri da 1 a 64). 
-
-// Ogni volta che clicco su un quadrato questo si colora di 
-// verde se il numero contenuto e pari e in rosso se dispari.
 document.getElementById('play').addEventListener('click', startGame);
 
 function startGame() {
@@ -17,18 +11,16 @@ function startGame() {
     }else{
         numberOfSquares = 49;
     }
-    
-    // Per ogni numero nell'array, creo una cella e la appendo al grid container
+   
+
     const mainGrid = document.getElementById('grid');
     mainGrid.innerHTML = '';
     for(let i = 1; i <= numberOfSquares; i++) {
         const thisNumber = [i];
         const newGeneratedSquare = generateGridItem(thisNumber);
 
-        // Attacco l'evento allo square
         newGeneratedSquare.addEventListener('click', handleSquareClick);
         
-        // Aggiungo l'elemento alla griglia
         mainGrid.appendChild(newGeneratedSquare);
 
         if ( scelta === 'Easy' ){
@@ -57,10 +49,10 @@ function generateGridItem(number) {
 function generateSquaresNumbers (quantityOfNumbers) {
     const numbersArray = [];
     while(numbersArray.length < quantityOfNumbers) {
-        // Un numero random
+
         const randomNumber = getRndInteger(1, quantityOfNumbers);
 
-        // Se il numero random non è gia presente in numbersArray lo pusho
+
         if( !numbersArray.includes(randomNumber) ) {
             numbersArray.push(randomNumber);
         }
